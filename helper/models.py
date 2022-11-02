4140c3a9343ebbcc3ef87f4d2fbfab8f1d9de0de
jashdoshi007 <doshijash3@gmail.com>
2022-11-02 00:43:28 -0400

    Basic structure

---
+++ helper/models.py
@@ -0,0 +1,38 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingClassifier
+
+
+
+def DecisionTree(data):
+    """
+    
+    """
+    model = DecisionTreeClassifier()
+    pass
+
+
+def LinearClassifier(data):
+    """
+    
+    """
+
+    model = LogisticRegression()
+    pass
+
+
+def GradientBoosted(data):
+    """
+    
+    """
+
+    pass
+
+
+def NeuralNets(data):
+    """
+    
+    """
+
+    pass
---
+++ helper/processing.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+
+
+def drop_columns(data, column_names):
+    """
+    
+    """
+
+    pass
+
+
+def label_encoder(data, column_names):
+    """
+    
+    """
+    pass
+
+
+
+def evaluation():
+    """
+    
+    """
+    pass
